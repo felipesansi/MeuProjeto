915b76db107c62a2a2d4b0bcea5c3996fffff5ba
Vivi634 <lealcampos2022@outlook.com>
2023-09-13 21:20:30 -0300

    Login

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native-elements';
+import { StyleSheet,  View } from 'react-native';
+import { Input,Button } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text h3>Login</Text>
+    <Input 
+      placeholder='E-MAIL'
+      leftIcon={{type:'font-awesome', name: 'envelope'}}
+      keyboordType="email-address"
+    />
+     <Input 
+      placeholder='Senha'
+      leftIcon={{type:'font-awesome', name: 'lock'}}
+      secureTextEntry={true}
+    />
+
+<Button
+              title="ENTRAR"
+              icon={{
+                name: 'check',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              />
+
     </View>
   );
 }
